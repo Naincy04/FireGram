a0d0d5d2d0dbcf1ec6aefa7f0e0f035bfcea6dd2
Naincy04 <naincy2102@gmail.com>
2024-03-08 19:08:58 +0530

    Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,11 @@ void main() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-          apiKey: "AIzaSyCZ-xrXqD5D19Snauto-Fx_nLD7PLrBXGM",
-          appId: "1:585119731880:web:eca6e4b3c42a755cee329d",
-          messagingSenderId: "585119731880",
-          projectId: "instagram-clone-4cea4",
-          storageBucket: 'instagram-clone-4cea4.appspot.com'),
+          apiKey: "AIzaSyA3nwUU2H60PuRNUGicSaT61NKejd3yfHg",
+          appId: "1:1059464434:web:fae506c6835717d9bb1c6b",
+          messagingSenderId: "1059464434",
+          projectId: "insta-clone-cb99f",
+          storageBucket: 'insta-clone-cb99f.appspot.com'),
     );
   } else {
     await Firebase.initializeApp();
